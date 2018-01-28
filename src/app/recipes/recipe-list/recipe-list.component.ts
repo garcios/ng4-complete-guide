2a96e9989e77bac75536cc1c187b8157f27bf666
a1633661 <oscarsalem.garcia@student.adelaide.edu.au>
2018-01-28 12:48:01 +0800

    added updates

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+   recipes:Recipe[] =[
+     new Recipe('A Test Receipe','This is simply a test','https://realfood.tesco.com/media/images/TescoWinter40-18GreekChicken-1400x919-f075adee-5ae5-4347-a14a-9ea69a482ce1-0-1400x919.jpg')
+   ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
