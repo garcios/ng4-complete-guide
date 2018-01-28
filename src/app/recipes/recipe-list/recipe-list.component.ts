c32be9bfa3d55123d756c453f1819e83bfc1b18d
a1633661 <oscarsalem.garcia@student.adelaide.edu.au>
2018-01-28 17:39:28 +0800

    added one more recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,8 @@ export class RecipeListComponent implements OnInit {
    @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
    recipes:Recipe[] =[
-     new Recipe('A Test Receipe','This is simply a test','https://realfood.tesco.com/media/images/TescoWinter40-18GreekChicken-1400x919-f075adee-5ae5-4347-a14a-9ea69a482ce1-0-1400x919.jpg')
+     new Recipe('A Test Recipe','This is simply a test','https://realfood.tesco.com/media/images/TescoWinter40-18GreekChicken-1400x919-f075adee-5ae5-4347-a14a-9ea69a482ce1-0-1400x919.jpg'),
+     new Recipe('Another Recipe','This is simply a test','https://assets.epicurious.com/photos/56bb4a1f2388d8216df8060b/2:1/w_1260%2Ch_630/EP_02052016_mindyschicken_recipe.jpg')
    ];
 
   constructor() { }
